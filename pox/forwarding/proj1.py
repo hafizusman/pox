3da31b8c6a3ec0eb9d678d7dcced691691f9bc29
Muhammad Usman <usman@uw.edu>
2013-10-28 16:25:14 -0700

    Final

--- pox/forwarding/proj1.py
+++ pox/forwarding/proj1.py
@@ -31,7 +31,11 @@ class L2LearningSwitch (object):
     packet = event.parsed
 
     def flood (message = None):
-      """ Floods the packet (doesn't send back to the in port though) """
+      """ 
+      This function floods the packet (doesn't send back to the in port since
+        we're using OFPP_FLOOD) 
+      """
+      # The ofp_packet_out message instructs a switch to send a packet.
       msg = of.ofp_packet_out()
 
       # Have we waited long enough to flood?
@@ -47,11 +51,12 @@ class L2LearningSwitch (object):
     def drop (duration = None):
       """
       This function will discard the packet and install a flow modification
-      rule on the controller 
+      rule on the controller in case of a timeout
       """
       if duration is not None:
         if not isinstance(duration, tuple):
           duration = (duration,duration)
+        log.debug("adding drop flow_mod")
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet)
         msg.idle_timeout = duration[0]
@@ -64,6 +69,7 @@ class L2LearningSwitch (object):
         msg.in_port = event.port
         self.connection.send(msg)
 
+    # Cache the mac-port mapping 
     self.macToPort[packet.src] = event.port
 
     # Drop LLDP packets 
@@ -72,25 +78,26 @@ class L2LearningSwitch (object):
       drop()
       return
 
+    # Multicast messages need to be sent to all ports
     if packet.dst.is_multicast:
       flood()
     else:
       if packet.dst not in self.macToPort:
-        flood("Port for %s unknown -- flooding" % (packet.dst,))
+        flood("Port for %s unknown" % (packet.dst,))
       else:
         port = self.macToPort[packet.dst]
         if port == event.port:
-          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
+          log.warning("Can't send to same port: %s -> %s on %s.%s.  Dropping..."
               % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
           drop(10)
           return
 
-        log.debug("flow_mod: for %s.%i -> %s.%i" % (packet.src, event.port, packet.dst, port))
+        # Update the controllers flow table
+        log.debug("adding flow_mod: for %s.%i -> %s.%i..." % (packet.src, event.port, packet.dst, port))
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet, event.port)
         msg.idle_timeout = 10
-        #doesn't seem like we need a hard timeout fow now
-        #msg.hard_timeout = 30
+        msg.hard_timeout = 30
         msg.actions.append(of.ofp_action_output(port = port))
         msg.data = event.ofp
         self.connection.send(msg)
