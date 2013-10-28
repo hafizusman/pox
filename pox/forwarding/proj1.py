97e1fb98e6910994429b988d25efb137ed10cb14
Muhammad Usman <usman@uw.edu>
2013-10-28 15:48:06 -0700

    Final draft v1

--- pox/forwarding/proj1.py
+++ pox/forwarding/proj1.py
@@ -1,78 +1,136 @@
 """
 Author Muhammad Usman
 
-This is an L2 learning switch written directly against the OpenFlow library.
-It is derived from 
-http://courses.cs.washington.edu/courses/csep561/13au/projects/L2LearningSwitch.txt.
+An L2 learning switch written directly against the OpenFlow library.
+Derived from http://courses.cs.washington.edu/courses/csep561/13au/projects/LearningSwitch.txt
 """
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.revent import *
-from pox.lib.util import dpidToStr
+from pox.lib.util import *
 import time
 
 log = core.getLogger()
 
-HARD_TIMEOUT = 30
-IDLE_TIMEOUT = 30
-class L2LearningSwitch (EventMixin):
+# We don't want to flood immediately when a switch connects.
+_flood_delay = 0
 
-  def __init__ (self, connection):
+class L2LearningSwitch (object):
+
+  def __init__ (self, connection, transparent):
     # Switch we'll be adding L2 learning switch capabilities to
     self.connection = connection
+    self.transparent = transparent
 
-    # Our table
+    # Our table that maps the MAC addresses to the port
     self.macToPort = {}
 
     # We want to hear PacketIn messages, so we listen
     # to the connection
     connection.addListeners(self)
 
+    # We just use this to know when to log a helpful message
+    self.hold_down_expired = _flood_delay == 0
 
   def _handle_PacketIn (self, event):
+    """
+    Handle packet in messages from the switch to implement above algorithm.
+    """
 
-    # parsing the input packet
-    packet = event.parse()
-    
-    # updating out mac to port mapping
-    
-    if packet.type == packet.LLDP_TYPE or packet.type == 0x86DD:
-      # Drop LLDP packets 
-      # Drop IPv6 packets
-      # send of command without actions
+    packet = event.parsed
 
+    def flood (message = None):
+      """ Floods the packet """
       msg = of.ofp_packet_out()
-      msg.buffer_id = event.ofp.buffer_id
+      if time.time() - self.connection.connect_time >= _flood_delay:
+        # Only flood if we've been connected for a little while...
+
+        if self.hold_down_expired is False:
+          # Oh yes it is!
+          self.hold_down_expired = True
+          log.info("%s: Flood hold-down expired -- flooding",
+              dpid_to_str(event.dpid))
+
+        if message is not None: log.debug(message)
+        # OFPP_FLOOD is optional; on some switches you may need to change
+        # this to OFPP_ALL.
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+      else:
+        pass
+      msg.data = event.ofp
       msg.in_port = event.port
       self.connection.send(msg)
-      return
 
-    log.debug("Port for %s unknown -- flooding" % (packet.dst,))
-    msg = of.ofp_packet_out()
-    msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-    msg.buffer_id = event.ofp.buffer_id
-    msg.in_port = event.port
-    self.connection.send(msg)
+    def drop (duration = None):
+      """
+      Drops this packet and optionally installs a flow to continue
+      dropping similar ones for a while
+      """
+      if duration is not None:
+        if not isinstance(duration, tuple):
+          duration = (duration,duration)
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.idle_timeout = duration[0]
+        msg.hard_timeout = duration[1]
+        msg.buffer_id = event.ofp.buffer_id
+        self.connection.send(msg)
+      elif event.ofp.buffer_id is not None:
+        msg = of.ofp_packet_out()
+        msg.buffer_id = event.ofp.buffer_id
+        msg.in_port = event.port
+        self.connection.send(msg)
 
-class l2_learning_switch (EventMixin):
-  def __init__(self):
-    self.listenTo(core.openflow)
+    self.macToPort[packet.src] = event.port # 1
 
-#
-# Event raised when the connection to an OpenFlow switch has been established
-#
+    if not self.transparent: # 2
+      if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
+        drop() # 2a
+        return
+
+    if packet.dst.is_multicast:
+      flood() # 3a
+    else:
+      if packet.dst not in self.macToPort: # 4
+        flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
+      else:
+        port = self.macToPort[packet.dst]
+        if port == event.port: # 5
+          # 5a
+          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
+              % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
+          drop(10)
+          return
+        # 6
+        log.debug("installing flow for %s.%i -> %s.%i" %
+                  (packet.src, event.port, packet.dst, port))
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet, event.port)
+        msg.idle_timeout = 10
+        msg.hard_timeout = 30
+        msg.actions.append(of.ofp_action_output(port = port))
+        msg.data = event.ofp # 6a
+        self.connection.send(msg)
+
+
+class l2_learning_switch (object):
+  """
+  Waits for OpenFlow switches to connect and makes them learning switches.
+  """
+  def __init__ (self, transparent):
+    core.openflow.addListeners(self)
+    self.transparent = transparent
+
   def _handle_ConnectionUp (self, event):
     log.debug("Connection %s" % (event.connection,))
-    L2LearningSwitch(event.connection)
+    L2LearningSwitch(event.connection, self.transparent)
 
-#
-# The launch function is called by POX to tell the component to initialize itself
-# We don't use any command line parameters for now
-#
-def launch ():
-  log.debug("*** L2 Learning Switch Started ***")
-
-  # Register the L2 learning switch
-  core.registerNew(l2_learning_switch)
-
+"""
+launch function is one that POX calls to tell the component to initialize itself
+We don't use any command line parameters for now
+"""
+def launch (transparent=False):
+  """
+  Register our L2 learning switch
+  """
+  core.registerNew(l2_learning_switch, str_to_bool(transparent))
