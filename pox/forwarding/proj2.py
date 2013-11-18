29e31b14210f0d989522a5bf68b40280b153f937
Muhammad Usman <usman@uw.edu>
2013-11-18 12:19:36 -0800

    cleanup

--- pox/forwarding/proj2.py
+++ pox/forwarding/proj2.py
@@ -3,9 +3,10 @@ Author Muhammad Usman
 
 This is an L3 load balancer written directly against the OpenFlow library.
 Derived from  James McCauley's load balancer
+Service Virtual IP = 10.0.0.250
 
 Usage example:
-  python pox.py log.level --DEBUG forwarding.proj2 --ip=10.0.0.250 --replicas="10.0.0.3,10.0.0.4"
+  python pox.py log.level --DEBUG forwarding.proj2 --replicas="10.0.0.3,10.0.0.4"
 
 """
 
@@ -24,120 +25,80 @@ from pox.lib.util import str_to_bool, dpid_to_str
 
 log = core.getLogger("l3loadbalancer")
 
-
-FLOW_IDLE_TIMEOUT = 10
-FLOW_MEMORY_TIMEOUT = 60 * 5
 SERVICE_MAC = "00:00:00:00:ff:ff"
 SERVICE_IP = "10.0.0.250"
 
+# How long do we wait for an probe reply (ARP) before we consider a replica dead?
+REPLICA_ARP_RETRIES = 3
+
+# How often do we send out probes?
+REPLICA_RETRY_FREQ = 5
 
 
+FLOW_IDLE_TIMEOUT = 10
+FLOW_MEMORY_TIMEOUT = 60 * 5
 
 
-class MemoryEntry (object):
-  """
-  Record for flows we are balancing
-
-
-  Table entries in the switch "remember" flows for a period of time, but
-  rather than set their expirations to some long value (potentially leading
-  to lots of rules for dead connections), we let them expire from the
-  switch relatively quickly and remember them here in the controller for
-  longer.
-
-
-  Another tactic would be to increase the timeouts on the switch and use
-  the Nicira extension which can match packets with FIN set to remove them
-  when the connection closes.
-  """
-  def __init__ (self, server, first_packet, client_port):
-    self.server = server
+"""
+  Holds data for the flow that we're load balancing. Flows are cached on
+  an MRU basis - if a flow hasn't been used for a while, it is forgotten
+"""
+class FlowInfo (object):
+  def __init__ (self, replica, first_packet, client_port):
+    self.replica = replica
     self.first_packet = first_packet
     self.client_port = client_port
     self.refresh()
 
-
   def refresh (self):
     self.timeout = time.time() + FLOW_MEMORY_TIMEOUT
-
 
   @property
   def is_expired (self):
     return time.time() > self.timeout
-
 
   @property
   def key1 (self):
     ethp = self.first_packet
     ipp = ethp.find('ipv4')
     tcpp = ethp.find('tcp')
-
-
     return ipp.srcip,ipp.dstip,tcpp.srcport,tcpp.dstport
-
 
   @property
   def key2 (self):
     ethp = self.first_packet
     ipp = ethp.find('ipv4')
     tcpp = ethp.find('tcp')
-
-
-    return self.server,ipp.srcip,tcpp.dstport,tcpp.srcport
+    return self.replica,ipp.srcip,tcpp.dstport,tcpp.srcport
 
 
 
 
 class l3loadbalancer (object):
   """
-  A simple IP load balancer
+  Traffic to the service's virtual IP will be redirected to one of the replicas
 
-
-  Give it a service_ip and a list of server IP addresses.  New TCP flows
-  to service_ip will be randomly redirected to one of the replicas.
-
-
-  We probe the replicas to see if they're alive by sending them ARPs.
+  Replica liveliness is checked using ARP requests.
   """
-  def __init__ (self, connection, service_ip, replicas = []):
-    self.service_ip = IPAddr(service_ip)
+  def __init__ (self, connection, replicas = []):
+    self.service_ip = IPAddr(SERVICE_IP)
+    self.mac = EthAddr(SERVICE_MAC)
     self.replicas = [IPAddr(a) for a in replicas]
     self.con = connection
-    self.mac = EthAddr(SERVICE_MAC)
-    self.live_servers = {} # IP -> MAC,port
-    self.live_server_index = 0
+    self.server_loaded_index = 0
+    self.servers_loaded = {}
 
-
-    try:
-      self.log = log.getChild(dpid_to_str(self.con.dpid))
-    except:
-      # Be nice to Python 2.6 (ugh)
-      self.log = log
-
-
-    self.outstanding_probes = {} # IP -> expire_time
-
-
-    # How quickly do we probe?
-    self.probe_cycle_time = 5
-
-
-    # How long do we wait for an ARP reply before we consider a server dead?
-    self.arp_timeout = 3
-
+    self.log = log.getChild(dpid_to_str(self.con.dpid))
+ 
+    # IP -> expire_time
+    self.outstanding_probes = {}
 
     # We remember where we directed flows so that if they start up again,
-    # we can send them to the same server if it's still up.  Alternate
+    # we can send them to the same replica if it's still up.  Alternate
     # approach: hashing.
-    self.memory = {} # (srcip,dstip,srcport,dstport) -> MemoryEntry
-
+    self.traffic_flows = {} # (srcip,dstip,srcport,dstport) -> FlowInfo
 
     self._do_probe() # Kick off the probing
-
-
-    # As part of a gross hack, we now do this from elsewhere
-    #self.con.addListeners(self)
-
 
   def _do_expire (self):
     """
@@ -148,149 +109,118 @@ class l3loadbalancer (object):
     """
     t = time.time()
 
-
     # Expire probes
     for ip,expire_at in self.outstanding_probes.items():
       if t > expire_at:
         self.outstanding_probes.pop(ip, None)
-        if ip in self.live_servers:
+        if ip in self.servers_loaded:
           self.log.warn("Server %s down", ip)
-          del self.live_servers[ip]
+          del self.servers_loaded[ip]
 
 
     # Expire old flows
-    c = len(self.memory)
-    self.memory = {k:v for k,v in self.memory.items()
+    c = len(self.traffic_flows)
+    self.traffic_flows = {k:v for k,v in self.traffic_flows.items()
                    if not v.is_expired}
-    if len(self.memory) != c:
-      self.log.debug("Expired %i flows", c-len(self.memory))
+    if len(self.traffic_flows) != c:
+      self.log.debug("Expired %i flows", c-len(self.traffic_flows))
 
 
   def _do_probe (self):
     """
-    Send an ARP to a server to see if it's still up
+    Sends arps to replica to see if it's still alive
     """
     self._do_expire()
 
-
-    server = self.replicas.pop(0)
-    self.replicas.append(server)
-
+    replica = self.replicas.pop(0)
+    self.replicas.append(replica)
 
     r = arp()
     r.hwtype = r.HW_TYPE_ETHERNET
     r.prototype = r.PROTO_TYPE_IP
     r.opcode = r.REQUEST
     r.hwdst = ETHER_BROADCAST
-    r.protodst = server
+    r.protodst = replica
     r.hwsrc = self.mac
     r.protosrc = self.service_ip
     e = ethernet(type=ethernet.ARP_TYPE, src=self.mac,
                  dst=ETHER_BROADCAST)
     e.set_payload(r)
-    #self.log.debug("ARPing for %s", server)
+
     msg = of.ofp_packet_out()
     msg.data = e.pack()
     msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
     msg.in_port = of.OFPP_NONE
     self.con.send(msg)
 
+    self.outstanding_probes[replica] = time.time() + REPLICA_ARP_RETRIES
+    r = REPLICA_RETRY_FREQ/ float(len(self.replicas))
+    r = REPLICA_RETRY_FREQ / float(len(self.replicas))
+    r = max(.25, r)
+    core.callDelayed(r, self._do_probe)
 
-    self.outstanding_probes[server] = time.time() + self.arp_timeout
 
-
-    core.callDelayed(self._probe_wait_time, self._do_probe)
-
-
-  @property
-  def _probe_wait_time (self):
+  def _next_server (self):
     """
-    Time to wait between probes
+    Pick a replica for load balancing in a round-robin fashion
     """
-    r = self.probe_cycle_time / float(len(self.replicas))
-    r = max(.25, r) # Cap it at four per second
-    return r
-
-
-  def _pick_server (self):
-    """
-    Pick a server for a (hopefully) new connection
-    """
-    keys = self.live_servers.keys()
-    n = len(keys)
-    picked_live_server = keys[self.live_server_index]
-    self.live_server_index += 1
-    if self.live_server_index >= n:
-      self.live_server_index = 0
-    return picked_live_server
+    keys = self.servers_loaded.keys()
+    picked_server = keys[self.server_loaded_index]
+    self.server_loaded_index += 1
+    if self.server_loaded_index >= len(keys):
+      self.server_loaded_index = 0
+    return picked_server
 
   def _handle_PacketIn (self, event):
     inport = event.port
     packet = event.parsed
 
-
     def drop ():
       if event.ofp.buffer_id is not None:
-        # Kill the buffer
         msg = of.ofp_packet_out(data = event.ofp)
         self.con.send(msg)
       return None
 
-
     tcpp = packet.find('tcp')
+
+    # handle arp requests for replica liveliness
     if not tcpp:
       arpp = packet.find('arp')
       if arpp:
-        # Handle replies to our server-liveness probes
+        # Let our liveliness arp requests know that we're still alive
         if arpp.opcode == arpp.REPLY:
           if arpp.protosrc in self.outstanding_probes:
-            # A server is (still?) up; cool.
+            # Replica is up, remove it from pending list
             del self.outstanding_probes[arpp.protosrc]
-            if (self.live_servers.get(arpp.protosrc, (None,None))
-                == (arpp.hwsrc,inport)):
-              # Ah, nothing new here.
-              pass
-            else:
-              # Ooh, new server.
-              self.live_servers[arpp.protosrc] = arpp.hwsrc,inport
-              self.log.info("Server %s up", arpp.protosrc)
+
+            if (self.servers_loaded.get(arpp.protosrc, (None,None))
+                != (arpp.hwsrc,inport)):
+              # A new replica has been discovered.
+              self.servers_loaded[arpp.protosrc] = arpp.hwsrc,inport
+              self.log.info("Replica %s is now online", arpp.protosrc)
         return
 
-
-      # Not TCP and not ARP.  Don't know what to do with this.  Drop it.
+      # Unhandled packet type, discard
       return drop()
 
-
-    # It's TCP.
-    
+    # handle higher layer packet
     ipp = packet.find('ipv4')
 
-
+    # Is it from one of the replicas, if so send it back to the client
     if ipp.srcip in self.replicas:
-      # It's FROM one of our balanced replicas.
-      # Rewrite it BACK to the client
-
-
       key = ipp.srcip,ipp.dstip,tcpp.srcport,tcpp.dstport
-      entry = self.memory.get(key)
-
+      entry = self.traffic_flows.get(key)
 
       if entry is None:
-        # We either didn't install it, or we forgot about it.
+        # We weren't talking to this client
         self.log.debug("No client for %s", key)
         return drop()
 
-
-      # Refresh time timeout and reinstall.
+      # Update timeout and reinstall.
       entry.refresh()
 
-
-      #self.log.debug("Install reverse flow for %s", key)
-
-
       # Install reverse table entry
-      mac,port = self.live_servers[entry.server]
-
+      mac,port = self.servers_loaded[entry.replica]
 
       actions = []
       actions.append(of.ofp_action_dl_addr.set_src(self.mac))
@@ -298,7 +228,6 @@ class l3loadbalancer (object):
       actions.append(of.ofp_action_output(port = entry.client_port))
       match = of.ofp_match.from_packet(packet, inport)
 
-
       msg = of.ofp_flow_mod(command=of.OFPFC_ADD,
                             idle_timeout=FLOW_IDLE_TIMEOUT,
                             hard_timeout=of.OFP_FLOW_PERMANENT,
@@ -307,42 +236,35 @@ class l3loadbalancer (object):
                             match=match)
       self.con.send(msg)
 
-
     elif ipp.dstip == self.service_ip:
-      # Ah, it's for our service IP and needs to be load balanced
-
-
-      # Do we already know this flow?
+      # Valid request for our virtial service IP. Will be redirected to a replica
       key = ipp.srcip,ipp.dstip,tcpp.srcport,tcpp.dstport
-      entry = self.memory.get(key)
-      if entry is None or entry.server not in self.live_servers:
-        # Don't know it (hopefully it's new!)
-        if len(self.live_servers) == 0:
-          self.log.warn("No replicas!")
+      entry = self.traffic_flows.get(key)
+
+      if entry is None or entry.replica not in self.servers_loaded:
+        # Are there any unloaded replicas to redirect this request to?
+        if len(self.servers_loaded) == 0:
+          self.log.warn("No unloaded replicas! Dropping packet")
           return drop()
 
-
-        # Pick a server for this flow
-        server = self._pick_server()
-        self.log.debug("Directing traffic to %s", server)
-        entry = MemoryEntry(server, packet, inport)
-        self.memory[entry.key1] = entry
-        self.memory[entry.key2] = entry
+        # Load balance
+        replica = self._next_server()
+        self.log.debug("Traffic from client %s directed to replica %s" % (ipp.srcip.toStr(), replica))
+        entry = FlowInfo(replica, packet, inport)
+        self.traffic_flows[entry.key1] = entry
+        self.traffic_flows[entry.key2] = entry
    
-      # Update timestamp
+      # Update timeout and reinstall
       entry.refresh()
 
-
-      # Set up table entry towards selected server
-      mac,port = self.live_servers[entry.server]
-
+      # Set up table entry towards selected replica
+      mac,port = self.servers_loaded[entry.replica]
 
       actions = []
       actions.append(of.ofp_action_dl_addr.set_dst(mac))
-      actions.append(of.ofp_action_nw_addr.set_dst(entry.server))
+      actions.append(of.ofp_action_nw_addr.set_dst(entry.replica))
       actions.append(of.ofp_action_output(port = port))
       match = of.ofp_match.from_packet(packet, inport)
-
 
       msg = of.ofp_flow_mod(command=of.OFPFC_ADD,
                             idle_timeout=FLOW_IDLE_TIMEOUT,
@@ -351,16 +273,12 @@ class l3loadbalancer (object):
                             actions=actions,
                             match=match)
       self.con.send(msg)
-
-
 
 
 # Remember which DPID we're operating on (first one to connect)
 _dpid = None
 
-
 def launch (replicas):
-  vip = IPAddr(SERVICE_IP)
   replicas = replicas.replace(","," ").split()
   replicas = [IPAddr(x) for x in replicas]
 
@@ -368,7 +286,7 @@ def launch (replicas):
     global _dpid
     if _dpid is None:
       log.info("Connection up for Load Balancer")
-      core.registerNew(l3loadbalancer, event.connection, IPAddr(vip), replicas)
+      core.registerNew(l3loadbalancer, event.connection, replicas)
       _dpid = event.dpid
 
     if _dpid != event.dpid:
